ed6c5932642b503462f725a5d7aa1ac99dccd7c2
racheldoyle-sys <rachel.doyle@hennozzles.com>
2026-07-30 23:35:45 -0400

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -1,7 +1,7 @@
 var simplemaps_namap_mapdata={
   main_settings: {
    //General settings
-	width: "900", //'700' or 'responsive'
+	width: "responsive", //'700' or 'responsive'
 	background_color: "#FFFFFF",
 	background_transparent: "yes",
 	border_color: "#ffffff",
@@ -11,7 +11,7 @@ var simplemaps_namap_mapdata={
 	state_hover_color: "#a5b9c4",
 	state_url: "",
 	border_size: "1.5",
-	all_states_inactive: "yes",
+	all_states_inactive: "no",
 	all_states_zoomable: "no",
 	
 	//Location defaults
